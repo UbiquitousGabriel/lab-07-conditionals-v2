b5b56350f50eae2237caacc6c57436c430707d65
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-10-11 21:20:02 -0400

    +spanish.py RESOLVED

---
+++ spanish.py
@@ -0,0 +1,12 @@
+spanglish = input("Give me a word in English.")
+if spanglish == "cat":
+    print("gato");
+
+elif spanglish == "dog":
+    print("perro");
+
+elif spanglish == "horse":
+    print("caballo");
+
+else:
+    print("no entiendo.")
